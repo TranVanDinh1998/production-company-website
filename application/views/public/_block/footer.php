579b8c6e3f9130beeeb95a0882e27577044dc2f3
becalonton <huyenthu.nguyen212@gmail.com>
2022-06-10 11:34:34 +0700

    June 10 fix bug footer
    Fix bug footer

--- application/views/public/_block/footer.php
+++ application/views/public/_block/footer.php
@@ -4,28 +4,18 @@
                 <div class="custom-grid">
                 <div class="row">
                 <!-- Footer logo -->
-                <div class="col l-6 m-6">
-                <h3 class="footer__heading footer__title txt-upper"> Công ty cổ phần thanh toán điện tử VNPT EPAY</h3>
+                
+                <div class="col l-10 m-10 c-12">
                 <a href="<?php echo base_url("home/index")?>" class="footer__logo"><img class="image-contain" src="<?php echo base_url("public/assets/images/footer_logo.png")?>"></a>
                 <h3 class="footer__heading footer__title txt-upper"> Công ty cổ phần thanh toán điện tử VNPT</h3>
-                </div>
-                <!-- Footer logo ends -->
-                         
-                </div>      
-                </div>      
-                <!-- Footer content -->
-                <div class="footer__content">
-                    <div class="custom-grid">
-                        <!-- Footer row -->
-                        <ul class="footer__list row">
-
+                <div class="footer__list row">
                             <!-- Footer column -->
-                            <li class="footer__item col l-3 m-6 c-12">
+                            <li class="footer__item col l-4 m-6 c-12">
                                 <div class="footer__info">
                                     <div class="footer__heading">
-                                        <h3 class="footer__title">Trụ sở hà nội</h3>
+                                        <h3 class="footer__title">Trụ sở Hà Nội</h3>
                                     </div>
-                                    <p class="txt-justify">Tầng 14, tòa nhà Handico, khu đô thị mới Mễ Trì Hạ, đường Phạm Hùng, quận Nam Từ Liêm, Hà Nội</p>
+                                    <p class="address xt-justify">Tầng 14, tòa nhà Handico, khu đô thị mới Mễ Trì Hạ, đường Phạm Hùng, quận Nam Từ Liêm, Hà Nội</p>
                                     <ul class="footer__contact">
                                         <li>
                                             <p>Điện thoại:</p>
@@ -38,15 +28,13 @@
                                     </ul>
                                 </div>
                             </li>
-                            <!-- Footer column ends -->
 
-                            <!-- Footer column -->
-                            <li class="logo-service footer__content-item col l-3 m-6 c-12">
+                            <li class="logo-service footer__content-item col l-4 m-6 c-12">
                                 <div class="footer__info">
                                     <div class="footer__heading">
                                         <h3 class="footer__title">Chi nhánh HCM</h3>
                                     </div>
-                                    <p class="txt-justify">Phòng 702 - 111D Lý Chính Thắng - Phường 7 - Quận 3 - Hồ Chí Minh
+                                    <p class="address txt-justify">Phòng 702 - 111D Lý Chính Thắng - Phường 7 - Quận 3 - Hồ Chí Minh
                                     </p>
                                     <ul class="footer__contact">
                                         <li>
@@ -60,53 +48,17 @@
                                     </ul>
                                 </div>
                             </li>
-                            <!-- Footer column ends -->
-                            <!-- Footer column -->
-                            <li class="footer__content-item col l-3 m-12 c-12">
 
-                                 <!-- Footer service -->
-                            <div class="d-flex justify-content-between align-items-center">
-                                        <a target="_blank" href="https://esan.vn" class="footer__link">
-                                            <img class="image-contain" src="<?php echo base_url("public/assets/images/service_logo_1.png")?>" alt="">
-                                        </a>
-                                        <a target="_blank" href="https://thanhtoan247.net.vn" class="footer__link">
-                                            <img class="image-contain" src="<?php echo base_url("public/assets/images/service_logo_2.png")?>" alt="">
-                                        </a>
-                                        <a target="_blank" href="https://shipantoan.vn/" class="footer__link">
-                                            <img class="image-contain" src="<?php echo base_url("public/assets/images/service_logo_3.png")?>" alt="">
-                                        </a>
-                            </div>
-                            <!-- Footer service ends -->
-                                <div class="bottom footer__info">
+                            <li class="footer__content-item col l-4 m-12 c-12">
+                                 <div class="bottom footer__info">
                                     <div class="footer__info">
-                                        
                                         <div class="footer__heading">
                                             <h3 class="footer__title">Hotline </h3>
                                             <h3 class="footer__title txt-upper txt-primary"> 19006470</h3>
                                         </div>
                                     </div>
-                     
-                                </div>
-                        </li>
 
-                        <li class="col l-3 m-12 c-12">
-                        <div class="certificate d-flex justify-content-between align-items-center">
-                    <div>
-                    <img class="image-contain" src="<?php echo base_url("public/assets/images/certificate_1.png")?>" alt="">
-                    </div>
-                    <div>
-                    <img class="image-contain" src="<?php echo base_url("public/assets/images/certificate_2.png")?>" alt="">
-                    </div>
-                    <div>
-                    <img class="image-contain" src="<?php echo base_url("public/assets/images/certificate_3.png")?>" alt="">
-                    </div>
-                            </div>
-                            <!-- Footer service ends -->
-                                <div class="bottom footer__info">
-                                    <div class="footer__info">
-                                       
-                                    </div>
-                                    <div class="footer__heading">
+                                <div class="footer__heading">
                                         <h3 class="footer__title txt-title weight-bold">Liên hệ</h3>
                                         <!-- Footer social -->
                                         <ul class="social__list">
@@ -117,24 +69,48 @@
                                                 <a target="_blank" href="https://www.linkedin.com/company/vnpt-epay" class="social__link"><i class="fab fa-linkedin"></i></a>
                                             </li>
                                         </ul>
-                                        <!-- Footer social ends-->
-                                    </div>
-                     
-                                </div>
 
-                        </li>
-                            <!-- Footer column ends -->
-                        </ul>
+                                <div class="footer__heading d-flex justify-content-between">
+                                        <a target="_blank" href="https://esan.vn" class="footer__link">
+                                            <img class="image-contain" src="<?php echo base_url("public/assets/images/service_logo_1.png")?>" alt="">
+                                        </a>
+                                        <a target="_blank" href="https://thanhtoan247.net.vn" class="footer__link">
+                                            <img class="image-contain" src="<?php echo base_url("public/assets/images/service_logo_2.png")?>" alt="">
+                                        </a>
+                                        <a target="_blank" href="https://shipantoan.vn/" class="footer__link">
+                                            <img class="image-contain" src="<?php echo base_url("public/assets/images/service_logo_3.png")?>" alt="">
+                                        </a>
+                                </div>
+                                </li>    
                         <!-- Footer list ends -->
+                    
                     </div>
                 </div>
+
+                <div class="col l-2 m-2 c-12">
+                <div class="certificate">
+                    <div>
+                    <img class="image-contain" src="<?php echo base_url("public/assets/images/certificate_1.png")?>" alt="">
+                    </div>
+                    <div>
+                    <img class="image-contain" src="<?php echo base_url("public/assets/images/certificate_2.png")?>" alt="">
+                    </div>
+                    <div>
+                    <img class="image-contain" src="<?php echo base_url("public/assets/images/certificate_3.png")?>" alt="">
+                    </div>
+                </div>
+                </div>
+                <!-- Footer logo ends -->
+                </div>       
+
+                </div>  
                 <!-- Footer content ends -->
-            </div>
+                </div>
             <!-- Footer copyright -->
             <div class="footer__copyright">
             <div class="custom-grid">
                 <div class="row">
-                <div class="col l-9 m-9 c-12 d-flex justify-content-between align-items-center">
+                <div class="col l-12 m-12 c-12 d-flex justify-content-between align-items-center">
                 <p>Giấy phép hoạt động cung ứng dịch vụ trung gian thanh toán số <span class="txt-weight-bold">21/GP-NHNN</span> do Ngân hàng nhà nước Việt Nam cấp ngày 22/01/2016.
                 </p>   
             </div>
