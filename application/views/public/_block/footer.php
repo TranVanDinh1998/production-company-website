470fecf2afe5286087f1591ac484dcc69c84c93c
becalonton <huyenthu.nguyen212@gmail.com>
2022-06-12 13:52:01 +0700

    June 12 fix bugs
    Fix footer + about us bugs

--- application/views/public/_block/footer.php
+++ application/views/public/_block/footer.php
@@ -5,90 +5,108 @@
                 <div class="row">
                 <!-- Footer logo -->
                 
-                <div class="col l-10 m-10 c-12">
+                <div class="col l-11 m-11 c-12">
                 <a href="<?php echo base_url("home/index")?>" class="footer__logo"><img class="image-contain" src="<?php echo base_url("public/assets/images/footer_logo.png")?>"></a>
-                <h3 class="footer__heading footer__title txt-upper"> Công ty cổ phần thanh toán điện tử VNPT</h3>
+                <h3 class="footer__title footer__heading__logo txt-upper"> Công ty cổ phần thanh toán điện tử VNPT</h3>
+
+                <div class="custom-grid">
                 <div class="footer__list row">
                             <!-- Footer column -->
-                            <li class="footer__item col l-4 m-6 c-12">
+                            <li class="footer__content-item col l-4 m-6 c-12">
                                 <div class="footer__info">
                                     <div class="footer__heading">
                                         <h3 class="footer__title">Trụ sở Hà Nội</h3>
+                                        <p class="address xt-justify">Tầng 14, tòa nhà Handico, Mễ Trì Hạ, đường Phạm Hùng, quận Nam Từ Liêm, Hà Nội</p>
                                     </div>
-                                    <p class="address xt-justify">Tầng 14, tòa nhà Handico, khu đô thị mới Mễ Trì Hạ, đường Phạm Hùng, quận Nam Từ Liêm, Hà Nội</p>
-                                    <ul class="footer__contact">
+                                   
+                                <div class="footer__item">
+                                <ul class="footer__contact">
                                         <li>
-                                            <p>Điện thoại:</p>
-                                            <p class="txt-title txt-weight-bold txt-primary">+84 (24) 3933 5133</p>
+                                            <p>Điện thoại:
+                                            <span class="txt-title txt-weight-bold txt-primary">+84 (24) 3933 5133</span> </p>
                                         </li>
                                         <li>
-                                            <p>FAX:</p>
-                                            <p class="txt-title txt-weight-bold txt-primary">+84 (24) 3933 5166</p>
+                                            <p>Fax:
+                                            <span class="txt-title txt-weight-bold txt-primary">+84 (24) 3933 5166</span> </p>
                                         </li>
-                                    </ul>
+                                </ul>
+                                </div>   
+                                 
                                 </div>
                             </li>
 
-                            <li class="logo-service footer__content-item col l-4 m-6 c-12">
+                            <li class="footer__content-item col l-4 m-6 c-12">
                                 <div class="footer__info">
                                     <div class="footer__heading">
                                         <h3 class="footer__title">Chi nhánh HCM</h3>
-                                    </div>
-                                    <p class="address txt-justify">Phòng 702 - 111D Lý Chính Thắng - Phường 7 - Quận 3 - Hồ Chí Minh
+                                        <p class="address txt-justify">Phòng 702, DC Tower 111D Lý Chính Thắng, Phường 7, Quận 3, Hồ Chí Minh
                                     </p>
+                                    </div>
+                                
+                                <div class="footer__item">
                                     <ul class="footer__contact">
                                         <li>
-                                            <p>Điện thoại:</p>
-                                            <p class="txt-title txt-weight-bold txt-primary">+84 (28) 3526 5969 </p>
+                                            <p>Điện thoại:
+                                            <span class="txt-title txt-weight-bold txt-primary">+84 (28) 3526 5969 </span> </p>
                                         </li>
                                         <li>
-                                            <p>FAX:</p>
-                                            <p class="txt-title txt-weight-bold txt-primary">+84 (28) 3844 3786 </p>
+                                            <p>Fax:
+                                            <span class="txt-title txt-weight-bold txt-primary">+84 (28) 3844 3786 </span> </p>
                                         </li>
                                     </ul>
+                                </div>    
+                                    
                                 </div>
                             </li>
 
-                            <li class="footer__content-item col l-4 m-12 c-12">
-                                 <div class="bottom footer__info">
-                                    <div class="footer__info">
+                            <li class="footer__content-item col l-4 m-4 c-12">
+
+                                 <div class="bottom">
+                                    <div class="">
                                         <div class="footer__heading">
                                             <h3 class="footer__title">Hotline </h3>
                                             <h3 class="footer__title txt-upper txt-primary"> 19006470</h3>
                                         </div>
                                     </div>
 
-                                <div class="footer__heading">
+                                    <div class="footer__heading">
                                         <h3 class="footer__title txt-title weight-bold">Liên hệ</h3>
                                         <!-- Footer social -->
-                                        <ul class="social__list">
+                                        <ul class="footer__title social__list">
                                             <li class="social__item">
-                                                <a target="_blank" href="https://www.facebook.com/vnptepay" class="social__link"><i class="fab fa-facebook"></i></a>
+                                                <a target="_blank" href="https://www.facebook.com/vnptepay" class="social__link facebook-icon"><i class="fab fa-facebook"></i></a>
                                             </li>
                                             <li class="social__item">
-                                                <a target="_blank" href="https://www.linkedin.com/company/vnpt-epay" class="social__link"><i class="fab fa-linkedin"></i></a>
+                                                <a target="_blank" href="https://www.linkedin.com/company/vnpt-epay" class="social__link linkedin-icon"><i class="fab fa-linkedin"></i></a>
+                                            </li>
+                                            <li class="social__item">
+                                                <a target="_blank" href="https://www.youtube.com/channel/UCd4KKuvHIXXFqh2TRJgUWGg" class="social__link youtube-icon"><i class="fab fa-youtube"></i></a>
                                             </li>
                                         </ul>
+                                    </div>
+                                </div>
 
-                                <div class="footer__heading d-flex justify-content-between">
+                                <div class="footer__heading__service">
+                                <div class="d-flex justify-content-between">
                                         <a target="_blank" href="https://esan.vn" class="footer__link">
                                             <img class="image-contain" src="<?php echo base_url("public/assets/images/service_logo_1.png")?>" alt="">
                                         </a>
                                         <a target="_blank" href="https://thanhtoan247.net.vn" class="footer__link">
                                             <img class="image-contain" src="<?php echo base_url("public/assets/images/service_logo_2.png")?>" alt="">
                                         </a>
-                                        <a target="_blank" href="https://shipantoan.vn/" class="footer__link">
+                                        <a target="_blank" href="https://shipantoan.vn" class="footer__link">
                                             <img class="image-contain" src="<?php echo base_url("public/assets/images/service_logo_3.png")?>" alt="">
                                         </a>
                                 </div>
-                                </li>    
+                                </div> 
+
+                            </li>    
                         <!-- Footer list ends -->
                     
                     </div>
                 </div>
-
-                <div class="col l-2 m-2 c-12">
-                <div class="certificate">
+                </div> 
+                <div class="certificate col l-1 m-1 c-12">
                     <div>
                     <img class="image-contain" src="<?php echo base_url("public/assets/images/certificate_1.png")?>" alt="">
                     </div>
@@ -99,9 +117,7 @@
                     <img class="image-contain" src="<?php echo base_url("public/assets/images/certificate_3.png")?>" alt="">
                     </div>
                 </div>
-                </div>
-                <!-- Footer logo ends -->
-                </div>       
+                      
 
                 </div>  
                 <!-- Footer content ends -->
